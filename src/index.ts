d2833c9af761689ac2e19ebe22e99c10bca62efb
vincentius.marco <vincentius.marco@createit.co.id>
2025-07-28 20:58:56 +0700

    add dockerfile

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import express from 'express';
-import type { Request, Response } from 'express';
+import express from "express";
+import type { Request, Response } from "express";
 
 const app = express();
 const port = 3000;
 
 app.get("/hello", (_req: Request, res: Response) => {
-    res.json({ message: 'Hello, world!' });
+  res.json({ message: "Hello, world!" });
 });
 
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
